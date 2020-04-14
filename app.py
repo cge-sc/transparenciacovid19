48d0ab4b28e0456cb4893e559e08df12a2bfbf62
Robson Junqueira <robsonjunqueira@Robsons-MacBook-Pro.local>
2020-04-14 17:48:23 -0300

    Versão Beta

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/consultas')
+def consultas():
+    return render_template('consultas.html')
+
+@app.route('/paineis')
+def paineis():
+    return render_template('paineis.html')
+
+@app.route('/saibamais')
+def saibamais():
+    return render_template('saibamais.html')
+
+@app.route('/busca')
+def busca():
+    return render_template('busca.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
